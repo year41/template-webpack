d6d5365f724236bc22becf63a38aeba29c858da1
qwerty <162951673+year41@users.noreply.github.com>
2026-07-02 13:10:22 +1000

    Add webpack-merge configuration.
    Divide webpack configuration in different files using webpack-merge.

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,12 @@
-// webpack.config.js
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/index.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,38 +1,7 @@
 // webpack.config.js
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
-export default {
+export default merge(common, {
     mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/index.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-};
+})
